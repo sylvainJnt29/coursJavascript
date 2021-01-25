908a02cf5450df7adcdae538d288d89d471856c5
sylvainJnt29 <sylvain.jnt@gmail.com>
2021-01-25 22:31:53 +0100

    25/01/2021

---
+++ bom.js
@@ -0,0 +1,48 @@
+//                                      BOM: Browser Object Model
+// Aperçu rapide du BOM
+
+
+//                                      WINDOW = objet global
+
+// https://codepen.io/believemy/full/XWJbZBw
+
+//https://developer.mozilla.org/fr/docs/Web/API/Window
+
+
+//                                      Navigator = informations sur le navigateur
+
+// https://codepen.io/believemy/full/XWJbELp
+
+// https://developer.mozilla.org/fr/docs/Web/API/Navigator
+
+
+//                                      History = historique :)
+
+// https://codepen.io/believemy/full/GRgJdNL
+
+// https://developer.mozilla.org/fr/docs/Web/API/History
+
+
+//                                      Location = emplacement de l'utilisateur ( url )
+
+// https://codepen.io/believemy/full/qBEdYyJ
+
+// https://developer.mozilla.org/fr/docs/Web/API/Location
+
+
+//                                      Screen
+
+// https://codepen.io/believemy/full/RwNPymw
+
+// https://developer.mozilla.org/fr/docs/Web/API/Window/screen
+
+
+//                                      Document
+
+// le plus utilisé
+
+
+
+
+
+
---
+++ dom.js
@@ -0,0 +1,24 @@
+//                                      DOM (Document Object Model)
+
+/* Premieres manipulations
+let header = document.getElementsByTagName("header");
+let logo = document.getElementById("logo");
+let container = document.getElementsByClassName("container");
+let h1 = document.querySelector("h1");// recupere une balise
+//let h1 = document.querySelector("#h1");// recupere un id
+//let h1 = document.querySelector(".h1");// recupere une class
+let p = document.querySelectorAll("p");
+
+
+console.log(header);
+console.log(logo);
+console.log(container);
+console.log(h1);
+console.log(p);
+*/
+
+//                                      Modifier le DOM
+
+let h1 = document.querySelector("h1");
+
+console.log(h1);
